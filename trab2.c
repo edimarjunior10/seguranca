d543bb43f91bcf31426a288144b998a795b21288
Edimar Roque Martello Júnior <edimar.junior.10@gmail.com>
2015-08-25 01:31:04 -0300

    Vegenere e substituição, faltanto transposição

--- trab2.c
+++ trab2.c
@@ -9,8 +9,8 @@ int main(){
 	
 	FILE *input, *output;
 	
-	input=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/inputs/pg11.txt", "r"); 
-	output=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/outputs/pg11.txt.enc", "r"); 
+	//input=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/inputs/pg11.txt", "r"); 
+	//output=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/outputs/pg11.txt.enc", "r"); 
 	
 	//input=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/inputs/pg74.txt", "r"); 
 	//output=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/outputs/pg74.txt.enc", "r"); 
@@ -30,8 +30,8 @@ int main(){
 	//input=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/inputs/pg1661.txt", "r"); 
 	//output=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/outputs/pg1661.txt.enc", "r"); 
 	
-	//input=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/inputs/pg27827.txt", "r"); 
-	//output=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/outputs/pg27827.txt.enc", "r"); 
+	input=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/inputs/pg27827.txt", "r"); 
+	output=fopen("/home/edimarjunior/Documentos/Gits/Seguranca/Trabalho2/arquivos/outputs/pg27827.txt.enc", "r"); 
 	
 	if (input==NULL){
 		printf("Arquivo Input nao encontrado!\n");
@@ -53,9 +53,10 @@ int main(){
     fseek(output, 0, 0);
     
     //VARIAVEIS-----------------------------------------------------
-    int tamTxtIn=0, tamTxtOut=0, op=0, x=0, y=0, w=0, z=0;
-    char txtIn[tamIn], txtOut[tamOut], txtAux[tamOut], txtAux2[tamOut], keyV[] = "BOLO";
-    int i=0, keyC=0, tamKv=0;
+    char txtIn[tamIn], txtOut[tamOut], txtAux[tamOut], keyV[tamOut]; //keyV[] = "abcd";
+    int tamTxtIn=0, tamTxtOut=0, op=0, ff=256, keyC=0, tamKv=0, keyT=0, loc=0;
+    int i=0, x=0, y=0, w=0, z=0, a, b, c, d, e, criaV;
+    char vsub[ff][2];
      
     while (!feof(input)){
 		fread(&txtIn[x], sizeof(char), 1, input);
@@ -91,15 +92,48 @@ int main(){
 	if(op==2){
 		//--------------------------------------------TRANSPOSICAO
 		//printf("\n\nTxtIn:\n %s", txtIn);
-		printf("\n\nTxtOut:\n %s", txtOut);
+		//printf("\n\nTxtOut:\n %s", txtOut);
+		//printf("tamArq: %d", tamOut);
+		//printf("tamStr: %d", tamTxtOut);
+		int multi=0;
+		
+		keyT = 7;
+		
+		loc = (tamTxtOut / keyT) +1;
+		
+		multi=loc*keyT;
+		
+		printf("tamOut: %d ", tamTxtOut);
+		printf("key: %d ", keyT);
+		printf("local: %d ", loc);
+		printf("multi: %d ", multi);
+		
+		int vish;
+		
+		for(vish=0;vish<tamTxtOut;vish++){
+			
+		}
+		
+		/*int la;
+		for(la=0; la<128; la++){
+			//printf("%d: %c", la, txtOut[la]);
+			printf("In: %d, %c ", txtIn[la],txtIn[la]);
+			printf(".Out:%d, %c\n ", txtOut[la],txtOut[la]);
+		} */
+		
+		
 		
 		
 	}
 	if(op==3){
 		//--------------------------------------------VEGENERE
-		//printf("\n\nTxtIn:\n %s", txtIn);
-		//printf("\n\nTxtOut:\n %s", txtOut);
+
+		/*int set=0;
+		for(set=0;set<ff;set++){
+			keyV[set] = '\0';
+		}
 		tamKv = strlen(keyV);
+		//printf("tamKv: %d", tamKv);
 		
 		z=0;
 		for(w=0; w<tamTxtOut; w++){
@@ -107,14 +141,18 @@ int main(){
 				z++;
 				if(z==tamKv)
 					z=0;
+		}*/	
+		
+		for(criaV=0; criaV<tamTxtOut; criaV++){
+			keyV[criaV] = txtOut[criaV] - txtIn[criaV];
 		}
+		printf("\n\nTxtAuxV:\n %s", keyV);
 		
 		for(i=0; i<tamTxtOut; i++){
-			txtAux2[i] = (txtOut[i] - txtAux[i] + 256) % 256;	
+			txtAux[i] = (txtOut[i] - keyV[i] + 256) % 256;	
 			//printf("keyC: %d\n", keyC);
 		}
-		
-		printf("\n\nTxtAuxV:\n %s", txtAux);
+		//printf("\n\nTxtAuxV:\n %s", txtAux);
 		
 		
 	}
@@ -122,6 +160,34 @@ int main(){
 		//--------------------------------------------SUBSTITUICAO
 		//printf("\n\nTxtIn:\n %s", txtIn);
 		//printf("\n\nTxtOut:\n %s", txtOut);
+		
+		for(e=0; e<tamTxtOut; e++){
+			txtAux[e] = txtOut[e];
+		}
+		//printf("\n\nTxtAux:\n %s", txtAux);
+
+		for(a=0; a<ff; a++){
+			vsub[a][0]=a;
+			for(b=0; b<tamTxtOut; b++){
+				if(vsub[a][0] == txtIn[b]){
+					vsub[a][1] = (txtOut[b] + 256) % 256;
+				}
+			}
+			//printf("0: %d %c  1: %d %c \n", vsub[a][0], vsub[a][0], vsub[a][1], vsub[a][1]);
+		}
+		
+		//printf("\n\nTxtDescrip:\n");
+		for(c=0; c<tamTxtOut; c++){			
+				for(d=0; d<ff; d++){
+					if(txtOut[c] == vsub[d][1]){
+						txtAux[c] = (vsub[d][0] + 256) % 256;
+						printf("%c", txtAux[c]);	
+					}
+				}
+			}
+		printf("\n%s", txtAux);
+		
+		
 	}
 	//}
 	
@@ -131,11 +197,11 @@ int main(){
 
 /*
 	Arquivo 1 - pg11 - CESAR - Chave:5
-	Arquivo 2 - pg74 - 
-	Arquivo 3 - pg76 - 
-	Arquivo 4 - pg174 - 
+	Arquivo 2 - pg74 - SUBSTITUIÇÃO
+	Arquivo 3 - pg76 - SUBSTITUIÇÃO
+	Arquivo 4 - pg174 - VEGENERE - Chave:abcd
 	Arquivo 5 - pg1232 - CESAR - Chave:22
-	Arquivo 6 - pg1342 - 
-	Arquivo 7 - pg1661 - 
-	Arquivo 8 - pg27827 - 
+	Arquivo 6 - pg1342 - 7
+	Arquivo 7 - pg1661 - 55
+	Arquivo 8 - pg27827 - VEGENERE - Chave:123456
 */
